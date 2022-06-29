33a340d69c97fe786320a5e977ca6c6d3b1c2345
Avaneet Agrahari <68479381+agraavi786@users.noreply.github.com>
2022-06-30 01:49:27 +0530

    Update String3.java

--- String3.java
+++ String3.java
@@ -7,14 +7,15 @@ public class String3 {
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
 		String s = sc.nextLine();
-		String r="";
+		//String r="";
 		for (int i=0;i<2;i++) {
-			r=r+s.charAt(i);
+			//r=r+s.charAt(i);
+			System.out.print(s.substring(0, 2));
 		}
 		
-		for(int i=0;i<s.length();i++) {
-		System.out.print(r);
-		}
+		//for(int i=0;i<s.length();i++) {
+		//System.out.print(r);
+		//}
 		
 
 	}
